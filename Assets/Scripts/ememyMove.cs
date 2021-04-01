db6e4ed6c788d199b3098c1402c65c363f8604a8
김형주 <zelodoc08@gmail.com>
2021-04-01 11:56:27 +0900

    no message

--- Assets/Scripts/ememyMove.cs
+++ Assets/Scripts/ememyMove.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ememyMove : MonoBehaviour
 {
     [SerializeField]
+    private bool isdamaged;
+    [SerializeField]
     private float speed;
     float hp=100;
     // Start is called before the first frame update
@@ -16,15 +18,29 @@ public class ememyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position+=Vector3.down*Time.deltaTime*speed;
+        //transform.position+=Vector3.down*Time.deltaTime*speed;
+        transform.Translate(Vector3.down*Time.deltaTime*speed);
     }
     private void OnTriggerEnter2D(Collider2D collision){
-        if (collision.CompareTag("Bullet")){
-            hp-=50;
-        }
-        if (hp<0){
+        if (collision.CompareTag("lazer")){
             Destroy(gameObject);
-            Destroy(collision.gameObject);
         }
+        if (collision.CompareTag("Bullet")){
+            Destroy(collision.gameObject);
+            if(hp>0){
+                if (isdamaged) return;
+                isdamaged=true;
+                StartCoroutine(damaged());
+                return;
+            }
+            //hp-=50;
+            
+            Destroy(gameObject);
+        }
+    }
+    private  IEnumerator damaged(){
+        hp-=50;
+        yield return new WaitForSeconds(0.1f);
+        isdamaged=false;
     }
 }
