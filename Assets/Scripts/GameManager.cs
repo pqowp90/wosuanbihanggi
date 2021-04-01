1cc43be0585f84162af957e71bec6faf3e908bd7
DESKTOP-OHD6DA9\USER <zelodoc08@gmail.com>
2021-04-02 00:34:17 +0900

    no message

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,17 +11,19 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawnCroissant());
     }
     private IEnumerator SpawnCroissant(){
-        float spawnDeley = 0f;
-        spawnDeley = Random.Range(2f,5f);
-        float RandomX = 0f;
-        RandomX = Random.Range(-7f,7f);
+        while(true){
+            float spawnDeley = 0f;
+            spawnDeley = Random.Range(2f,5f);
+            float RandomX = 0f;
+            RandomX = Random.Range(-7f,7f);
 
-
-        for(int i=0;i<6;i++){
-            yield return new WaitForSeconds(0.2f);
-            Instantiate(enemyCroissantPrefab, new Vector2(RandomX,20f),Quaternion.identity);
+        
+            for(int i=0;i<6;i++){
+                yield return new WaitForSeconds(0.2f);
+                Instantiate(enemyCroissantPrefab, new Vector2(RandomX,20f),Quaternion.identity);
+            }
+            yield return new WaitForSeconds(spawnDeley);
         }
-        yield return new WaitForSeconds(spawnDeley);
     }
 
 
