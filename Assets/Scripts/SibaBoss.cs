1fc56c54fba8e2b5c6546dad1fb741a30daa7939
DESKTOP-OHD6DA9\USER <zelodoc08@gmail.com>
2021-05-10 20:34:02 +0900

    no message

--- Assets/Scripts/SibaBoss.cs
+++ Assets/Scripts/SibaBoss.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class SibaBoss : MonoBehaviour
 {
     [SerializeField]
     private GameObject bulletPrefab;
+    private float rotationZ = 0f;
+    [SerializeField]
+    private Slider hpBar;
     [SerializeField]
     private Sprite NextSprite1;
     [SerializeField]
@@ -14,6 +18,9 @@ public class SibaBoss : MonoBehaviour
     float duration=2f,strength=0.7f;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+    private Vector2 diff = Vector2.zero;
+    private float bossHp=200f;
+    GameObject bullet;
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -24,22 +31,103 @@ public class SibaBoss : MonoBehaviour
     {
         
     }
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Bullet")){
+            Destroy(other);
+            bossHp--;
+            if(bossHp<=0){
+
+            }
+            hpBar.value = bossHp/200f;
+        }
+    }
     public void StartBoss(){
         StartCoroutine(Boss());
     }
     private  IEnumerator Boss(){
         yield return new WaitForSeconds(3.3f);
         animator.enabled=false;
+        hpBar.gameObject.SetActive(true);
+        StartCoroutine(BossAttack11());
+        yield return new WaitForSeconds(2.5f);
+        StartCoroutine(BossAttack3());
+        yield return new WaitForSeconds(2.5f);
+        while(true){
+            int random;
+            random = Random.Range(0,3);
+            switch(random){
+                case 0:
+                StartCoroutine(BossAttack1());
+                break;
+                case 1:
+                StartCoroutine(BossAttack2());
+                break;
+                case 2:
+                StartCoroutine(BossAttack3());
+                break;
+            }
+            yield return new WaitForSeconds(2.5f);
+        }
+        
+
+        
+
+        
+    }
+
+    private  IEnumerator Die(){
         spriteRenderer.sprite = NextSprite2;
         transform.DOShakePosition(duration,strength,18,90f,false,false);
         for(int i=0;i<10;i++){
-            GameObject bullet = Instantiate(bulletPrefab);
-            bullet.transform.rotation = new Quaternion(0f,0f,100f+i*(150f/10f),0f);
+             bullet = Instantiate(bulletPrefab);
             bullet.transform.position= transform.position;
-            yield return new WaitForSeconds(2f/10f);
+            bullet.transform.rotation = Quaternion.Euler(0f,0f,100f+i*(150f/10f));
+            yield return new WaitForSeconds(2f/20f);
         }
         spriteRenderer.sprite = NextSprite1;
-
-        
+    }
+    private  IEnumerator BossAttack1(){
+        spriteRenderer.sprite = NextSprite2;
+        transform.DOShakePosition(duration,strength,18,90f,false,false);
+        for(int i=0;i<20;i++){
+             bullet = Instantiate(bulletPrefab);
+            bullet.transform.position= transform.position;
+            bullet.transform.rotation = Quaternion.Euler(0f,0f,100f+i*(150f/10f));
+            yield return new WaitForSeconds(2f/20f);
+        }
+        spriteRenderer.sprite = NextSprite1;
+    }
+    private  IEnumerator BossAttack2(){
+        spriteRenderer.sprite = NextSprite2;
+        transform.DOShakePosition(duration,strength,18,90f,false,false);
+        for(int i=0;i<20;i++){
+             bullet = Instantiate(bulletPrefab);
+            bullet.transform.position= transform.position;
+            bullet.transform.rotation = Quaternion.Euler(0f,0f,(i<10)?100f+i*(150f/10f):250-(i-10)*(150f/10f));
+            yield return new WaitForSeconds(2f/20f);
+        }
+        spriteRenderer.sprite = NextSprite1;
+    }
+    private  IEnumerator BossAttack3(){
+        spriteRenderer.sprite = NextSprite2;
+        transform.DOShakePosition(duration,strength,18,90f,false,false);
+        for(int i=0;i<20;i++){
+             bullet = Instantiate(bulletPrefab);
+            bullet.transform.position= transform.position;
+            bullet.transform.rotation = Quaternion.Euler(0f,0f,100f+Random.Range(0f,150f));
+            yield return new WaitForSeconds(2f/20f);
+        }
+        spriteRenderer.sprite = NextSprite1;
+    }
+    private  IEnumerator BossAttack11(){
+        while(true){
+            bullet = Instantiate(bulletPrefab,transform);
+            diff = GameManager.Instance.player.transform.position - transform.position;
+            diff.Normalize();
+            rotationZ = Mathf.Atan2(diff.y,diff.x)*Mathf.Rad2Deg;
+            bullet.transform.rotation = Quaternion.Euler(0f,0f,rotationZ-90f);
+            bullet.transform.parent=null;
+            yield return new WaitForSeconds(0.3f);
+        }
     }
 }
